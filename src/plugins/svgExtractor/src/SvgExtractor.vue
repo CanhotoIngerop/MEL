9dc589b5976c89dc1697b379bc1841ad638a8ad3
Vincent DAVIN <vincent.davin@ingerop.com>
2022-05-09 10:27:00 +0200

    Initial Commit

---
+++ src/plugins/svgExtractor/src/SvgExtractor.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- https://vuejs.org/v2/guide/syntax.html -->
+  <div>
+    <BIMDataButton @click="downloadSvg" color="primary" fill radius>{{
+      $t("SvgExtractorPlugin.action")
+    }}</BIMDataButton>
+  </div>
+</template>
+
+<script>
+import { BIMDataButton } from "@bimdata/design-system/components.js";
+
+export default {
+  // https://vuejs.org/v2/guide/components.html
+  name: "svgExtractor",
+  components: {
+    BIMDataButton,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    downloadSvg() {
+      const ifc = this.$viewer.state.ifcs[0];
+      if (ifc.map_file) {
+        window.location.href = ifc.map_file;
+      } else {
+        this.$viewer.localContext.hub.emit("alert", {
+          type: "error",
+          message: this.$t("SvgExtractorPlugin.no_svg_error"),
+        });
+      }
+    },
+  },
+};
+</script>
---
+++ src/views/Viewer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="viewer">
+    <div :id="viewerId"></div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import makeBIMDataViewer from "@bimdata/viewer";
+
+import backgroundColor from "@/plugins/backgroundColor/src/backgroundColor.plugin.js";
+import BimObjectPlugin from "@/plugins/bimobject/src/bimobject.plugin.js";
+import bsdd from "@/plugins/bsdd/src/bsdd.plugin.js";
+import ChristmasSleighPlugin from "@/plugins/christmasSleigh/src/christmasSleigh.plugin.js";
+import GltfExtractorPlugin from "@/plugins/gltfExtractor/src/gltfExtractor.plugin.js";
+import HolusionPlugin from "@/plugins/holusion/src/holusion.plugin.js";
+import kroqiBcfService from "@/plugins/kroqiBcfService/src/kroqiBcfService.plugin.js";
+import platformDemo from "@/plugins/platformDemo/src/platformDemo.plugin.js";
+import SnowflakesPlugin from "@/plugins/snowflakes/src/snowflakes.plugin.js";
+import SplitPlugin from "@/plugins/split/src/split.plugin.js";
+import SvgExtractorPlugin from "@/plugins/svgExtractor/src/svgExtractor.plugin.js";
+
+export default {
+  data() {
+    return {
+      viewerId: "bimdataViewerId",
+    };
+  },
+  computed: {
+    ...mapGetters(["oidcAccessToken"]),
+  },
+  mounted() {
+    const bimdataViewer = makeBIMDataViewer({
+      locale: "fr",
+      api: {
+        cloudId: this.$route.query.cloudId,
+        projectId: this.$route.query.projectId,
+        ifcIds: [this.$route.query.ifcId],
+        apiUrl: process.env.VUE_APP_BIMDATA_API_URL,
+        accessToken: this.oidcAccessToken,
+      },
+      plugins: {
+        bcfKroqiPremiumService: {
+          kroqiDomain: "pfptnbdev.io",
+          organization: "cstb",
+          userIsAdmin: true,
+          kroqiProjectId: "123456789",
+          service: "BIMDATA",
+        },
+      },
+    });
+
+    bimdataViewer.registerPlugin(backgroundColor);
+    bimdataViewer.registerPlugin(BimObjectPlugin);
+    bimdataViewer.registerPlugin(bsdd);
+    bimdataViewer.registerPlugin(ChristmasSleighPlugin);
+    bimdataViewer.registerPlugin(GltfExtractorPlugin);
+    bimdataViewer.registerPlugin(HolusionPlugin);
+    bimdataViewer.registerPlugin(kroqiBcfService);
+    bimdataViewer.registerPlugin(platformDemo);
+    bimdataViewer.registerPlugin(SnowflakesPlugin);
+    bimdataViewer.registerPlugin(SplitPlugin);
+    bimdataViewer.registerPlugin(SvgExtractorPlugin);
+
+    bimdataViewer.mount(`#${this.viewerId}`);
+
+    this.$watch(
+      () => this.oidcAccessToken,
+      token => {
+        bimdataViewer.setAccessToken(token);
+      }
+    );
+  },
+};
+</script>
+
+<style scoped>
+.viewer {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
