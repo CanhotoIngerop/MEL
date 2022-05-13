033013eac91d005af28659ae7eba4f29fbc038f3
Anthony Bertrand <anthony.bertrand@ingerop.com>
2022-05-13 16:33:04 +0200

    'Le CSS est la plus belle invention au monde !' N'a jamais dis la moindre personne dans l'UNIVERS

--- src/plugins/vueGlobale/src/VueGlobale.vue
+++ src/plugins/vueGlobale/src/VueGlobale.vue
@@ -48,7 +48,28 @@
       <div id="calendrier" style="display: none;">
         <div class="text-center section">
           <h2 class="h2">Calendrier</h2>
-          <Calendar :attributes='attrs'>
+          <Calendar
+            class="custom-calendar max-w-full"
+            :masks="masks"
+            :attributes="attributes"
+            disable-page-swipe
+            is-expanded
+          >
+            <template v-slot:day-content="{ day, attributes }">
+              <div class="vc-container flex flex-col h-full z-10 overflow-hidden">
+                <span class="day-label text-sm text-gray-900">{{ day.day }}</span>
+                <div class="flex-grow overflow-y-auto overflow-x-auto ">
+                  <p
+                    v-for="attr in attributes"
+                    :key="attr.key"
+                    class="text-xs leading-tight rounded-sm p-1 mt-0 mb-1"
+                    :class="attr.customData.class"
+                  >
+                    {{ attr.customData.title }}
+                  </p>
+                </div>
+              </div>
+            </template>
           </Calendar>
         </div>
       </div>
@@ -159,17 +180,84 @@ export default {
           categories: [],
         },
       },
-      attrs: [
+      masks: {
+        weekdays: 'WWW',
+      },
+      attributes: [
         {
-          key: 'today',
-          highlight: {
-            backgroundColor: '#ff8080',
-            // Other properties are available too, like `height` & `borderRadius`
+          key: 1,
+          customData: {
+            title: "Contrôle",
+            class: "event",
           },
-          dates: new Date(2018, 0, 1)
-        }
+          dates: new Date(year, month, 1),
+        },
+        {
+          key: 2,
+          customData: {
+            title: "Visite",
+            class: "event",
+          },
+          dates: new Date(year, month, 2),
+        },
+        {
+          key: 3,
+          customData: {
+            title: "Visite",
+            class: "event",
+          },
+          dates: new Date(year, month, 5),
+        },
+        {
+          key: 4,
+          customData: {
+            title: "Contrôle",
+            class: "event",
+          },
+          dates: new Date(year, month, 5),
+        },
+        {
+          key: 4,
+          customData: {
+            title: "Contrôle",
+            class: "event",
+          },
+          dates: new Date(year, month, 7),
+        },
+        {
+          key: 5,
+          customData: {
+            title: "Visite",
+            class: "event",
+          },
+          dates: new Date(year, month, 11),
+        },
+        {
+          key: 6,
+          customData: {
+            title: "Réunion",
+            class: "event",
+          },
+          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
+        },
+        {
+          key: 7,
+          customData: {
+            title: "Réunion",
+            class: "event",
+          },
+          dates: new Date(year, month, 22),
+        },
+        {
+          key: 8,
+          customData: {
+            title: "Réunion",
+            class: "event",
+          },
+          dates: new Date(year, month, 25),
+        },
       ],
-    }
+    };
   },
   created() {
     this.getNiveau();
@@ -238,58 +326,28 @@ export default {
   }
 }
 
-.webkit-scrollbar {
+
+$day-border: 1px solid #b8c2cc;
+$day-border-highlight: 1px solid #b8c2cc;
+$day-width: 90px;
+$day-height: 90px;
+$weekday-bg: #f8fafc;
+$weekday-border: 1px solid #eaeaea;
+.scrollbar {
   width: 0px;
 }
-.webkit-scrollbar-track {
+.scrollbar-track {
   display: none;
 }
 .custom-calendar.vc-container {
-  --day-border: 1px solid #b8c2cc;
-  --day-border-highlight: 1px solid #b8c2cc;
-  --day-width: 90px;
-  --day-height: 90px;
-  --weekday-bg: #f8fafc;
-  --weekday-border: 1px solid #eaeaea;
   border-radius: 0;
   width: 100%;
-  & .vc-header {
-    background-color: #f1f5f8;
-    padding: 10px 0;
-  }
-  & .vc-weeks {
-    padding: 0;
-  }
-  & .vc-weekday {
-    background-color: var(--weekday-bg);
-    border-bottom: var(--weekday-border);
-    border-top: var(--weekday-border);
-    padding: 5px 0;
-  }
-  & .vc-day {
-    padding: 0 5px 3px 5px;
-    text-align: left;
-    height: var(--day-height);
-    min-width: var(--day-width);
-    background-color: white;
-    &.weekday-1,
-    &.weekday-7 {
-      background-color: #eff8ff;
-    }
-    &:not(.on-bottom) {
-      border-bottom: var(--day-border);
-      &.weekday-1 {
-        border-bottom: var(--day-border-highlight);
-      }
-    }
-    &:not(.on-right) {
-      border-right: var(--day-border);
-    }
-  }
-  & .vc-day-dots {
-    margin-bottom: 5px;
-  }
 }
 
-/* https://vue-loader.vuejs.org/guide/scoped-css.html#mixing-local-and-global-styles */
+.event{
+  background: blue;
+  color: white;
+  width: 100px;
+  height: 50px;
+}
 </style>
