3c78126ec49cf966066ebc2819bdff2125f1b941
Anthony Bertrand <anthony.bertrand@ingerop.com>
2022-05-16 11:53:14 +0200

    css qui fonctionne aux 3/4. Doit trouver le moyen de cliquer sur un jour pour afficher toutes les alertes

--- src/plugins/vueGlobale/src/VueGlobale.vue
+++ src/plugins/vueGlobale/src/VueGlobale.vue
@@ -22,7 +22,7 @@
 
     
     <div id="tabCtrl">
-      <div class="mainDiv" id="localisation">
+      <div class="mainDiv" id="localisation" style="display: none;">
         <div class="BarDiv">
           <apexcharts class="BarGraph" height="350" type="bar" :options="chartOptionsBar" :series="seriesBarNonConform"></apexcharts>
           <apexcharts class="BarGraph" height="350" type="bar" :options="chartOptionsBar" :series="seriesBarDesord"></apexcharts>
@@ -45,7 +45,7 @@
         Corps d'etat ici
       </div>
 
-      <div id="calendrier" style="display: none;">
+      <div id="calendrier" style="display: block;">
         <div class="text-center section">
           <h2 class="h2">Calendrier</h2>
           <Calendar
@@ -59,18 +59,21 @@
               <div class="vc-container flex flex-col h-full z-10 overflow-hidden">
                 <span class="day-label text-sm text-gray-900">{{ day.day }}</span>
                 <div class="flex-grow overflow-y-auto overflow-x-auto ">
-                  <p
+                  <div
                     v-for="attr in attributes"
                     :key="attr.key"
                     class="text-xs leading-tight rounded-sm p-1 mt-0 mb-1"
                     :class="attr.customData.class"
                   >
                     {{ attr.customData.title }}
-                  </p>
+                  </div>
                 </div>
               </div>
             </template>
           </Calendar>
+        </div>
+        <div class="detail-day">
+          
         </div>
       </div>
     </div>
@@ -81,7 +84,6 @@
 <script>
 import VueApexCharts from 'vue-apexcharts';
 import Calendar from 'v-calendar/lib/components/calendar.umd'
-import DatePicker from 'v-calendar/lib/components/date-picker.umd'
 export default {
   
   // https://vuejs.org/v2/guide/components.html
@@ -187,74 +189,38 @@ export default {
         {
           key: 1,
           customData: {
-            title: "Contrôle",
-            class: "event",
-          },
-          dates: new Date(year, month, 1),
-        },
-        {
-          key: 2,
-          customData: {
-            title: "Visite",
-            class: "event",
+            title: "Contrôle prévu",
+            class: "rappel",
+            description: "Controle prévu le 16/05/2022",
           },
           dates: new Date(year, month, 2),
         },
         {
+          key: 2,
+          customData: {
+            title: "Contrôle",
+            class: "urgent",
+            description: "Visite sur le chantier de Ladoux",
+          },
+          dates: new Date(year, month, 16),
+        },
+        {
           key: 3,
           customData: {
-            title: "Visite",
-            class: "event",
+            title: "conforme",
+            class: "fait",
+            description: "escalier mécanique conforme",
           },
           dates: new Date(year, month, 5),
         },
         {
           key: 4,
           customData: {
-            title: "Contrôle",
-            class: "event",
+            title: "Contrôle prévu",
+            class: "rappel",
+            description: "Controle prévu le 30/05/2022",
           },
-          dates: new Date(year, month, 5),
-        },
-        {
-          key: 4,
-          customData: {
-            title: "Contrôle",
-            class: "event",
-          },
-          dates: new Date(year, month, 7),
-        },
-        {
-          key: 5,
-          customData: {
-            title: "Visite",
-            class: "event",
-          },
-          dates: new Date(year, month, 11),
-        },
-        {
-          key: 6,
-          customData: {
-            title: "Réunion",
-            class: "event",
-          },
-          dates: { months: 5, ordinalWeekdays: { 2: 1 } },
-        },
-        {
-          key: 7,
-          customData: {
-            title: "Réunion",
-            class: "event",
-          },
-          dates: new Date(year, month, 22),
-        },
-        {
-          key: 8,
-          customData: {
-            title: "Réunion",
-            class: "event",
-          },
-          dates: new Date(year, month, 25),
+          dates: new Date(year, month, 16),
         },
       ],
     };
@@ -292,6 +258,9 @@ export default {
           node.style.display = (node == pageToActivate) ? 'block' : 'none';
         }
       }
+    },
+    detail(key){
+      console.log(key);
     }
   },
 };
@@ -318,7 +287,7 @@ export default {
 }
 .vueGlobale-plugin {
   height: 100%;
-  background-color: var(--color-white);
+  background-color: white;
 
   &__header {
     min-height: 44px;
@@ -326,28 +295,73 @@ export default {
   }
 }
 
-
-$day-border: 1px solid #b8c2cc;
-$day-border-highlight: 1px solid #b8c2cc;
-$day-width: 90px;
-$day-height: 90px;
-$weekday-bg: #f8fafc;
-$weekday-border: 1px solid #eaeaea;
-.scrollbar {
+::-webkit-scrollbar {
   width: 0px;
 }
-.scrollbar-track {
+::-webkit-scrollbar-track {
   display: none;
 }
-.custom-calendar.vc-container {
+
+::v-deep .custom-calendar.vc-container {
+  --day-border: 1px solid #b8c2cc;
+  --day-border-highlight: 1px solid #b8c2cc;
+  --day-width: 90px;
+  --day-height: 90px;
+  --weekday-bg: #f8fafc;
+  --weekend-bg: #eff8ff;
+  --weekday-border: 1px solid #eaeaea;
   border-radius: 0;
   width: 100%;
-}
+  & .vc-header {
+    background-color: #f1f5f8;
+    padding: 10px 0;
+  }
+  & .vc-weeks {
+    padding: 0;
+  }
+  & .vc-weekday {
+    background-color: var(--weekday-bg);
+    border-bottom: var(--weekday-border);
+    border-top: var(--weekday-border);
+    padding: 5px 0;
+  }
+  & .vc-day {
+    padding: 0 5px 3px 5px;
+    text-align: left;
+    height: var(--day-height);
+    min-width: var(--day-width);
+    background-color: white;
+    &.weekday-1,
+    &.weekday-7 {
+      background-color: var(--weekend-bg);
+      & .day-label{
+        background-color: var(--weekend-bg);
+      }
+    }
+    &:not(.on-bottom) {
+      border-bottom: var(--day-border);
+      &.weekday-1 {
+        border-bottom: var(--day-border-highlight);
+      }
+    }
+    &:not(.on-right) {
+      border-right: var(--day-border);
+    }
+    
+  }
+  & .vc-day-dots {
+    margin-bottom: 5px;
+  }
 
-.event{
-  background: blue;
-  color: white;
-  width: 100px;
-  height: 50px;
+  & .rappel{
+    background-color: yellow;
+  }
+  & .urgent{
+    background-color: red;
+  }
+  & .fait{
+    background-color: green;
+  }
+  
 }
 </style>
